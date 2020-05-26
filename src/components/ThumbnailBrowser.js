602c232adbb9cc7b176ac26a7f74bf33d1c9e384
Jay Glickman <jayglickman@gmail.com>
2020-05-26 19:57:55 -0400

    lots of css

--- src/components/ThumbnailBrowser.js
+++ src/components/ThumbnailBrowser.js
@@ -12,18 +12,27 @@ class ThumbnailBrowser extends Component{
     componentDidMount() {
         //  this.props.clearImagesFromState //need to reset images in state before adding more to
          // this.props.readFromLocalStorage("", 25)
-         $('.refresh-icon-div').click(this.props.getDropboxFileSearch)
+         $('#re-sync').click(this.props.getDropboxFileSearch)
+         $('#random').click(() =>{
+             this.props.clearImagesFromState()
+             this.props.readFromLocalStorage("")
+         })
     }
 
     render () {
         
         return (
-
-            <div className='thumb-browser'>
-                <h3 className="thumbnail-heading">Browse Thumbnails</h3>
-                    <div className="refresh-icon-div">
-                        <button title="re-sync Dropbox library"></button>
+            <>
+                <div className='thumb-browser-header'>
+                    <h3 className="thumbnail-heading">Browse Thumbnails</h3>
+                    <div className="click-button-div">
+                        <button id="re-sync" title="re-sync Dropbox library"></button>
+                        <button id="random" title="show 25 random images"></button>
                     </div>
+                </div>
+                <div className='thumb-browser'>
+                
+
                 {this.props.thumbnailArray ? 
                     this.props.thumbnailArray.map(thumbnail =>
                     <a key={thumbnail.imageId} href="#page_header">
@@ -37,6 +46,7 @@ class ThumbnailBrowser extends Component{
                     )
                 : null }
             </div>
+            </>
         )
     }
     
